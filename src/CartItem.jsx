2c65da73546b8e29ed45916b6b5f492c320daf98
Anna <coffeeplant@abramek.art>
2025-10-09 09:23:36 +0200

    add cart buttons handling

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -17,9 +17,12 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  }
 
 
   const handleIncrement = (item) => {
@@ -61,7 +64,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
